49f1eb3f0fcfe49b12237a3857a1760f3fc51cd2
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2012-06-10 12:21:48 -0400

    wrote generator function to computer prime factors of a number in python

--- Python/generators.py
+++ Python/generators.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def genrange(x, y=None, z=None):
     """generator version of the range() function"""
     if y == None:
@@ -11,3 +13,18 @@ def genrange(x, y=None, z=None):
         yield start
         start += step
 
+
+def prime_factors(n):
+    """generator function prime_factors returns each prime factor of a numer"""
+    if n < 2:
+        return
+    else:
+        i = 2
+        limit = n ** 0.5
+        while i <= limit:
+             if not n % i:
+                 yield i
+                 n = n / i
+                 limit = n ** 0.5
+             else:
+                 i += 1

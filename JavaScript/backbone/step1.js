aa0673110fa44aabcd8053cf5ff1892f3e10f41c
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2012-08-15 17:10:07 -0400

    addStatus now uses the options parameter rather then hardcoding the dom maniuplations

--- JavaScript/backbone/step1.js
+++ JavaScript/backbone/step1.js
@@ -22,10 +22,8 @@ function addStatus(options){
         url: '/status',
         type: 'POST',
         dataType: 'json',
-        data: { text: $('#new-status textarea').val() } // options.data
-        success: function(data) { // options.value
-            $('#statuses ul').append('<li>' + data.text + '</li>');
-            $('#new-status textarea').val('');
+        data: { options.text },// options.data
+        success: options.success
         }
     });
 }
@@ -34,6 +32,12 @@ function addStatus(options){
 $(document).ready(function() {
     $('#new-status form').submit(function(e) {
         e.preventDefault();
-        addStatus();
+        addStatus({
+            text: $('#new-status textarea').val(),
+            success: function(data) {
+                $('#statuses ul').append('<li>' + data.text + '</li>');
+                $('new-statuses textarea').val('');
+            }
+        });
     });
 });

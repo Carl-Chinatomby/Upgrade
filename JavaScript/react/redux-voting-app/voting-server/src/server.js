3a53234f68b1a001ce7cd8d2ad72bd41ece444ac
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2015-12-06 21:53:40 -0500

    got green deploys yay!

--- JavaScript/react/redux-voting-app/voting-server/src/server.js
+++ JavaScript/react/redux-voting-app/voting-server/src/server.js
@@ -1,7 +1,7 @@
 import Server from 'socket.io';
 
 export default function startServer(store) {
-  const io = new Server().attach(8080);
+  const io = new Server().attach(8090);
 
   store.subscribe(
     () => io.emit('state', store.getState().toJS())

648786a9056dfa5d93dc93fd0ae97e207905b8a2
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2017-12-15 23:11:41 -0500

    Python performance profiling scripts

---
+++ Python/performance_profiling/julia_set_inefficient.py
@@ -0,0 +1,83 @@
+"""Julia set generator without optional PIL-based image drawing"""
+import time
+from functools import wraps
+
+# area of complex space to investigate
+x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
+c_real, c_image = -0.62772, -.42193
+
+
+def timefn(fn):
+    @wraps(fn)
+    def measure_time(*args, **kwargs):
+        t1 = time.time()
+        result = fn(*args, **kwargs)
+        t2 = time.time()
+        print("@timefn:" + fn.__name__  + " took " + str(t2 - t1) + " seconds")
+        return result
+    return measure_time
+
+
+@timefn
+#@profile # used for line_profiler (kernprof)
+def calculate_z_serial_purepython(maxiter, zs, cs):
+    """Calculate output list using Julia update rule"""
+    output = [0] * len(zs)
+    for i in range(len(zs)):
+        n = 0
+        z = zs[i]
+        c = cs[i]
+        while n < maxiter and abs(z) < 2:
+            z = z * z + c
+            n += 1
+        output[i] = n
+    return output
+
+
+def calc_pure_python(desired_width, max_iterations):
+    """Create a list of complex coordiantes (zs) and complex
+    parameters (cs), build Julia set, and display"""
+    x_step = (float(x2 - x1) / float(desired_width))
+    y_step = (float(y1 - y2) / float(desired_width))
+    x = []
+    y = []
+    ycoord = y2
+    while ycoord > y1:
+        y.append(ycoord)
+        ycoord += y_step
+    xcoord = x1
+    while xcoord < x2:
+        x.append(xcoord)
+        xcoord += x_step
+    # Build a list of coordinates and the intial condition for each cell
+    # Note that our intital condition is a constant and could easily be removed;
+    # We use it to simulate a real-world scenerio with several inputs to
+    # our function.
+    zs = []
+    cs = []
+    for ycoord in y:
+        for xcoord in x:
+            zs.append(complex(xcoord, ycoord))
+            cs.append(complex(c_real, c_image))
+
+    print("Length of x:", len(x))
+    print("Total Elements:", len(zs))
+    start_time = time.time()
+    output = calculate_z_serial_purepython(max_iterations, zs, cs)
+    end_time = time.time()
+    secs = end_time - start_time
+    print(calculate_z_serial_purepython.__name__ + " took", secs, "seconds")
+
+    # This sum is expected for a 1000^2 grid with 300 iterations.
+    # It catches minor errors we might introduce when we're
+    # working on a fixed set of inputs.
+    assert sum(output) == 33219980
+
+
+
+
+
+if __name__ == "__main__":
+    # Calculate the Julia set using a pure Python solution with
+    # reasonable defaults for a laptop
+    calc_pure_python(desired_width=1000, max_iterations=300)
